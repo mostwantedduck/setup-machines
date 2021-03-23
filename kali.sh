2c498f8b8497e928d53095b199b2780821c51c30
Most Wanted Duck <81170836+mostwantedduck@users.noreply.github.com>
2021-03-23 16:22:15 +0100

    Create kali.sh

---
+++ kali.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+sudo apt -y update
+sudo apt -y install iptables-persistent netfilter-persistent python3-pip steghide rlwrap
+
+sudo systemctl disable network-manager.service
+echo -en "\n\nauto eth0\niface eth0 inet dhcp\nauto eth1\niface eth1 inet static\n\taddress 192.168.10.100\n\tnetmask 255.255.255.0" | sudo tee -a /etc/network/interfaces
+sudo service networking restart
+
+sudo sed -i "s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g" /etc/sysctl.conf
+
+sudo iptables -t nat -A POSTROUTING -o tun0 -j MASQUERADE
+sudo netfilter-persistent save
+sudo systemctl enable netfilter-persistent.service
+
+wget -q https://packages.microsoft.com/config/ubuntu/19.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt -y update
+sudo apt -y install apt-transport-https
+sudo apt -y update
+sudo apt -y install dotnet-sdk-3.1 dnsutils
+rm packages-microsoft-prod.deb
+
+sudo git clone --recurse-submodules https://github.com/ZeroPointSecurity/Covenant.git /opt/Covenant
+sudo git clone https://github.com/rbsec/dnscan.git /opt/dnscan
+sudo git clone https://github.com/chinarulezzz/spoofcheck /opt/spoofcheck; cd /opt/spoofcheck; sudo pip3 install -r requirements.txt
+sudo git clone https://gist.github.com/superkojiman/11076951 /opt/namemash; sudo chmod +x /opt/namemash/namemash.py
+sudo git clone https://github.com/byt3bl33d3r/SprayingToolkit.git /opt/SprayingToolkit; cd /opt/SprayingToolkit; sudo pip3 install -r requirements.txt
+sudo git clone https://github.com/FortyNorthSecurity/Egress-Assess.git /opt/Egress-Assess
+sudo git clone https://github.com/pentestmonkey/windows-privesc-check /opt/windows-privesc-check
+sudo git clone https://github.com/carlospolop/privilege-escalation-awesome-scripts-suite /opt/privilege-escalation-awesome-scripts-suite
+sudo git clone https://github.com/AonCyberLabs/Windows-Exploit-Suggester /opt/Windows-Exploit-Suggester
+sudo git clone git clone https://github.com/mzet-/linux-exploit-suggester /opt/linux-exploit-suggester
+sudo git clone https://github.com/jondonas/linux-exploit-suggester-2 /opt/linux-exploit-suggester-2
+sudo git clone https://github.com/PowerShellMafia/PowerSploit /opt/PowerSploit
+sudo git clone https://github.com/internetwache/GitTools /opt/GitTools
+sudo git clone https://github.com/maurosoria/dirsearch /opt/dirsearch
+sudo git clone https://github.com/rebootuser/LinEnum /opt/LinEnum
+sudo git clone https://github.com/diego-treitos/linux-smart-enumeration /opt/linux-smart-enumeration
+
+chmod +x /opt/dirsearch/dirsearch.py
+sudo ln -s /opt/dirsearch/dirsearch.py /bin/dirsearch
+
+sudo gem install evil-winrm
+
+sudo systemctl enable ssh.service
+
+sudo reboot
